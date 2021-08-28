321e0cbaaa9333385b0413259e2a6c3c6c4fa18a
JeffHauckJr <jefferyhauckjr@gmail.com>
2021-08-27 19:37:48 -0500

    Greet message working. Morking on message commands.

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
+//1 Bot needs to greet new members
+//search for new members and greet them as they join
+
+//2 Bot needs to respond when tagged
+//search for bots name being tagged
+
+//3 Bot needs to be connected with weather api 
+//search for keyword "weather" 
+
+//4 Bot can be connected with other api based on question
+//search for other keywords that may apply
+
+
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS],
 });
 const TOKEN = process.env.TOKEN;
+const WEATHERTOKEN = process.env.WEATHERTOKEN
 
 
 client.on("ready", () => {
-    console.info(`Logged in as ${client.user.tag}!`);
-  });
-  
+  console.info(`Logged in as ${client.user.tag}!`);
+});
 
-client.on("messageCreate", (msg) => {
-    if (msg.content.includes(`${client.user.tag}`)) {
-        console.log(client.user.tag)
-        msg.send(`How can I help you master ${client.user.tag}`)
-    }
+const prefix = '!'
+
+const channelId = "880527083909025793"
+client.on("guildMemberAdd", (member) => {
+  console.log(member)
+  const message = `Welcome Master ${member.user}`
+  const channel = member.guild.channels.cache.get(channelId)
+  channel.send(message)
 })
 
+client.on("messageCreate", (msg) => {
+  if(msg.content[0] !== prefix) {
+    console.log('no prefix')
+    return 
+  }
+  const args = msg.content.slice(prefix.length).trim().split(' ')
+  console.log(args)
+  const command = args.shift().toLowerCase()
+  console.log(command)
+})
 
-
-
-
-
-client.login(TOKEN)
+client.login(TOKEN);
